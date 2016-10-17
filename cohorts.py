980861ed4a7628b6badd4477088c9a7bdc30e2b4
Erik Bernhardsson <erikbern@spotify.com>
2016-10-16 20:35:45 -0400

    plot survival rate of code

--- cohorts.py
+++ cohorts.py
@@ -10,13 +10,18 @@ commit2cohort = {}
 commits = [] # only stores a subset
 bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
 last_date = None
+commit2timestamp = {}
+cohorts_set = set()
+
 for i, commit in enumerate(repo.iter_commits('master')):
     bar.update(i)
     cohort = datetime.datetime.utcfromtimestamp(commit.committed_date).strftime(fm)
     commit2cohort[commit.hexsha] = cohort
+    cohorts_set.add(cohort)
     if last_date is None or commit.committed_date < last_date - interval:
         commits.append(commit)
         last_date = commit.committed_date
+        commit2timestamp[commit.hexsha] = commit.committed_date
 
 def get_file_histogram(commit, path):
     h = {}
@@ -24,6 +29,8 @@ def get_file_histogram(commit, path):
         for old_commit, lines in repo.blame(commit, entry.path):
             cohort = commit2cohort[old_commit.hexsha]
             h[cohort] = h.get(cohort, 0) + len(lines)
+            if old_commit.hexsha in commit2timestamp:
+                h[old_commit.hexsha] = h.get(old_commit.hexsha, 0) + len(lines)
     except KeyboardInterrupt:
         raise
     except:
@@ -34,6 +41,7 @@ curves = {}
 ts = []
 file_histograms = {}
 last_commit = None
+commit_history = {}
 for commit in reversed(commits):
     t = datetime.datetime.utcfromtimestamp(commit.committed_date)
     ts.append(t)
@@ -54,19 +62,53 @@ for commit in reversed(commits):
         bar.update(i)
         if entry.path in changed_files or entry.path not in file_histograms:
             file_histograms[entry.path] = get_file_histogram(commit, entry.path)
-        for cohort, count in file_histograms[entry.path].items():
-            histogram[cohort] = histogram.get(cohort, 0) + count
-            curves.setdefault(cohort, [])
+        for key, count in file_histograms[entry.path].items():
+            histogram[key] = histogram.get(key, 0) + count
 
-    for cohort, curve in curves.items():
-        curve.append(histogram.get(cohort, 0))
+    for key, count in histogram.items():
+        if key not in cohorts_set:
+            commit_history.setdefault(key, []).append((commit.committed_date, count))
 
-print('redrawing cohort plot...')
-cohorts = list(sorted(curves.keys()))
-y = numpy.array([[0] * (len(ts) - len(curves[cohort])) + curves[cohort] for cohort in cohorts])
+    for cohort in cohorts_set:
+        curves.setdefault(cohort, []).append(histogram.get(cohort, 0))
+
+print('drawing cohort plot...')
+cohorts = sorted(cohorts_set)
+y = numpy.array([curves[cohort] for cohort in cohorts])
 pyplot.clf()
 pyplot.stackplot(ts, y, labels=['Code added in %s' % c for c in cohorts])
 pyplot.legend(loc=2)
 pyplot.ylabel('Lines of code')
 pyplot.savefig('cohorts.png')
 
+print('drawing survival chart')
+deltas = []
+total_n = 0
+for commit, history in commit_history.items():
+    t0, orig_count = history[0]
+    total_n += orig_count
+    last_count = orig_count
+    for t, count in history[1:]:
+        deltas.append((t-t0, count-last_count, 0))
+        last_count = count
+    deltas.append((time.time() - t0, -last_count, -orig_count))
+
+deltas.sort()
+total_k = total_n
+xs = []
+ys = []
+for t, delta_k, delta_n in deltas:
+    if t > 3 * 365.25 * 24 * 60 * 60:
+        break
+    xs.append(t / (365.25 * 24 * 60 * 60))
+    ys.append(100. * total_k / total_n)
+    total_k += delta_k
+    total_n += delta_n
+
+pyplot.clf()
+pyplot.plot(xs, ys)
+pyplot.xlabel('Years')
+pyplot.ylabel('%')
+pyplot.ylim([0, 100])
+pyplot.title('% of commit still present in code base over time')
+pyplot.savefig('survival.png')
