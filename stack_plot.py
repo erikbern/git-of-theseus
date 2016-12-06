080fd67bd5179a779eab254d7f227d2c50d88505
Erik Bernhardsson <erikbern@better.com>
2016-12-06 17:20:42 -0500

    fix bug introduced in #5

--- stack_plot.py
+++ stack_plot.py
@@ -7,7 +7,7 @@ parser.add_argument('--outfile', default='stack_plot.png', help='Output file to 
 parser.add_argument('inputs', nargs=1)
 args = parser.parse_args()
 
-data = json.load(open(args.inputs))
+data = json.load(open(args.inputs[0]))
 y = numpy.array(data['y'])
 pyplot.figure(figsize=(13, 8))
 pyplot.stackplot([dateutil.parser.parse(t) for t in data['ts']],
