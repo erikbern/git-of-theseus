c6c995183e3a4bd7f5c1b0d0cbd148b13221f546
Erik Bernhardsson <erikbern@better.com>
2018-01-04 14:14:47 -0500

    remove requirements.txt, move it into setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,12 @@ setup(name='git-of-theseus',
       url='https://github.com/erikbern/git-of-theseus',
       packages=['git_of_theseus'],
       scripts=['scripts/git-of-theseus-analyze', 'scripts/git-of-theseus-stack-plot', 'scripts/git-of-theseus-survival-plot'],
-      install_requires=[line.strip() for line in open('requirements.txt')]
+      install_requires=[
+          'gitpython',
+          'numpy',
+          'progressbar2',
+          'pygments',
+          'matplotlib',
+          'seaborn',
+      ]
 )
