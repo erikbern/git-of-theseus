b6d08c6bef460c4c428c2f34a1b415287f56588e
Erik Bernhardsson <mail@erikbern.com>
2018-01-04 14:39:08 -0500

    remove requirements.txt, move it into setup.py (#39)
    * remove requirements.txt, move it into setup.py
    
    * bump to 0.1.1

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from distutils.core import setup
 
 setup(name='git-of-theseus',
-      version='0.1.0',
+      version='0.1.1',
       description='Plot stats on Git repositories',
       author='Erik Bernhardsson',
       author_email='mail@erikbern.com',
       url='https://github.com/erikbern/git-of-theseus',
       packages=['git_of_theseus'],
       scripts=['scripts/git-of-theseus-analyze', 'scripts/git-of-theseus-stack-plot', 'scripts/git-of-theseus-survival-plot'],
-      install_requires=[line.strip() for line in open('requirements.txt')]
+      install_requires=[
+          'gitpython',
+          'numpy',
+          'progressbar2',
+          'pygments',
+          'matplotlib',
+          'seaborn',
+      ]
 )
