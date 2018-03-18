539ac36f9b55675f9cf69fc12280eeb1fe44d9e3
Erik Bernhardsson <mail@erikbern.com>
2018-03-17 21:43:50 -0400

    v0.1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='git-of-theseus',
-      version='0.1.2',
+      version='0.1.3',
       description='Plot stats on Git repositories',
       author='Erik Bernhardsson',
       author_email='mail@erikbern.com',
