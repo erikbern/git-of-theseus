fa927b74d4ae8456ab9f7b9c33919dd6571b3288
Erik Bernhardsson <mail@erikbern.com>
2018-05-13 14:10:52 -0400

    drop scripts in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(name='git-of-theseus',
       author_email='mail@erikbern.com',
       url='https://github.com/erikbern/git-of-theseus',
       packages=['git_of_theseus'],
-      scripts=['scripts/git-of-theseus-analyze', 'scripts/git-of-theseus-stack-plot', 'scripts/git-of-theseus-survival-plot'],
       install_requires=[
           'gitpython',
           'numpy',
