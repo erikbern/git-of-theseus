dfe62c4b96c6b5fa74aa7613c2c4a225af8e8db7
Erik Bernhardsson <mail@erikbern.com>
2018-05-26 11:41:29 -0400

    emoji progressbars #musthave

--- git_of_theseus/analyze.py
+++ git_of_theseus/analyze.py
@@ -25,6 +25,8 @@ for _, _, filetypes, _ in pygments.lexers.get_all_lexers():
     default_filetypes.update(filetypes)
 default_filetypes.difference_update(IGNORE_PYGMENTS_FILETYPES)
 
+widget_kwargs = dict(samples=10000, marker='\U0001f30a', right='\u26f5', left='\U0001f32c', markers=''.join(chr(0x1f311 + i) for i in range(8)))
+
 
 def analyze(repo, cohortfm='%Y', interval=7*24*60*60, ignore=[], only=[], outdir='.', branch='master', all_filetypes=False):
     repo = git.Repo(repo)
@@ -37,7 +39,7 @@ def analyze(repo, cohortfm='%Y', interval=7*24*60*60, ignore=[], only=[], outdir
         os.makedirs(outdir)
 
     print('Listing all commits')
-    with progressbar.ProgressBar(max_value=progressbar.UnknownLength) as bar:
+    with progressbar.ProgressBar(max_value=progressbar.UnknownLength, widget_kwargs=widget_kwargs) as bar:
         for i, commit in enumerate(repo.iter_commits(branch)):
             bar.update(i)
             cohort = datetime.datetime.utcfromtimestamp(commit.committed_date).strftime(cohortfm)
@@ -50,7 +52,7 @@ def analyze(repo, cohortfm='%Y', interval=7*24*60*60, ignore=[], only=[], outdir
                 commit2timestamp[commit.hexsha] = commit.committed_date
 
     print('Backtracking the master branch')
-    with progressbar.ProgressBar(max_value=progressbar.UnknownLength) as bar:
+    with progressbar.ProgressBar(max_value=progressbar.UnknownLength, widget_kwargs=widget_kwargs) as bar:
         i, commit = 0, repo.head.commit
         last_date = None
         while True:
@@ -79,7 +81,7 @@ def analyze(repo, cohortfm='%Y', interval=7*24*60*60, ignore=[], only=[], outdir
 
     print('Counting total entries to analyze + caching filenames')
     entries_total = 0
-    with progressbar.ProgressBar(max_value=len(master_commits)) as bar:
+    with progressbar.ProgressBar(max_value=len(master_commits), widget_kwargs=widget_kwargs) as bar:
         for i, commit in enumerate(reversed(master_commits)):
             bar.update(i)
             n = 0
@@ -115,7 +117,7 @@ def analyze(repo, cohortfm='%Y', interval=7*24*60*60, ignore=[], only=[], outdir
     commit_history = {}
 
     print('Analyzing commit history')
-    with progressbar.ProgressBar(max_value=entries_total, widget_kwargs=dict(samples=10000)) as bar:
+    with progressbar.ProgressBar(max_value=entries_total, widget_kwargs=widget_kwargs) as bar:
         entries_processed = 0
         for commit in reversed(master_commits):
             t = datetime.datetime.utcfromtimestamp(commit.committed_date)
